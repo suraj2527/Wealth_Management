49322ec124191cd925d8a29e2b11ce1ec2e4774a
suraj2527 <surajmalik2527@gmail.com>
2025-07-28 15:21:41 +0530

    .

--- lib/screens/mainscreens/splash_screen.dart
+++ lib/screens/mainscreens/splash_screen.dart
@@ -7,7 +7,7 @@ import 'package:wealth_app/controllers/auth_controller.dart';
 import 'package:wealth_app/screens/Authentication/login_screen.dart';
 import 'package:wealth_app/screens/mainscreens/dashboard_screen.dart';
 import 'package:wealth_app/widgets/dot_loader.dart';
-import 'package:wealth_app/extension/theme_extension.dart'; 
+import 'package:wealth_app/extension/theme_extension.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,7 +16,8 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
   final AuthController authController = Get.put(AuthController());
 
   final String fullText = 'Wealth Management';
