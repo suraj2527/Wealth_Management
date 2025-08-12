ee7221795eb0691ecab49339870a101bed4b4229
suraj2527 <surajmalik2527@gmail.com>
2025-08-12 17:24:38 +0530

    .

--- lib/presentation/screens/mainscreens/splash_screen.dart
+++ lib/presentation/screens/mainscreens/splash_screen.dart
@@ -37,7 +37,7 @@ class _SplashScreenState extends State<SplashScreen>
     _setupLogoFade();
     _startTextAnimation();
     checkLoginStatus();
-    // dashboardController.initializeDashboard(userId);
+    dashboardController.initializeDashboard(userId);
 
   }
 
